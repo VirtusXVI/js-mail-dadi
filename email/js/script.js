522be799efe179156ba377d2b92b7ee8b7742293
VirtusXVI <manuel.aveta2001@gmail.com>
2022-06-08 17:56:58 +0200

    checkpoint 1

--- email/js/script.js
+++ email/js/script.js
@@ -14,7 +14,7 @@ button.addEventListener( "click" , function(){
             finalOutput = `<h1 class="">La tua email è presente e quindi puoi accedere</h1>`
             console.log(output);
             break;
-        }else{
+        }else if(userEmail != emails[i] && i === emails.length - 1){
             // output negativo
             finalOutput = `<h1 class="">email non presente</h1>`
             console.log(output);
