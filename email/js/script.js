3b101698c4b0c9790e58c84e939d91413e40b6d1
VirtusXVI <manuel.aveta2001@gmail.com>
2022-06-08 14:59:25 +0200

    completed emails

---
+++ email/js/script.js
@@ -0,0 +1,27 @@
+
+// email che hanno il permesso di accesso
+let emails = [ "ciao1@ciao.it" , "ciao2@ciao.it" , "ciao3@ciao.it"];
+let button = document.querySelector("button");
+let output = document.querySelector("h1")
+// evento on click
+button.addEventListener( "click" , function(){
+    let userEmail = document.getElementById("email").value;
+    let finalOutput = "";
+    console.log(userEmail);
+    // controllo che la mail sia presente tra quelle con permesso
+    for(let i = 0 ; i < emails.length ; i++){
+        if(userEmail === emails[i]){
+            // output positivo
+            finalOutput = `<h1 class="">La tua email è presente e quindi puoi accedere</h1>`
+            console.log(output);
+            break;
+        }else{
+            // output negativo
+            finalOutput = `<h1 class="">email non presente</h1>`
+            console.log(output);
+            break;
+        }
+    }
+    document.getElementById("output").innerHTML = finalOutput
+}
+);
