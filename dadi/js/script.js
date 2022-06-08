1c1ef9939994557bea5fd16c2f477720b05ccf5e
VirtusXVI <manuel.aveta2001@gmail.com>
2022-06-08 15:09:19 +0200

    added comments

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -2,11 +2,12 @@
 let userNumber = Math.floor(Math.random() * 6);
 let computerNumber = Math.floor(Math.random() * 6);
 let ouput = "";
-
+// controllo se ha vinto il computer o l'utente
 if(userNumber > computerNumber){
-    output = `il giocatore ha vinto!`
+    output = `il giocatore ha vinto!`;
 }else{
-    output = `il computer ha vinto!`
-}
-console.log(output)
-alert(output)
+    output = `il computer ha vinto!`;
+};
+// output
+console.log(output);
+alert(output);
